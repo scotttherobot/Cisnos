7795816fa10396e0844077312c28731662b63246
Scott Vanderlind <scottv@ifixit.com>
2014-05-13 14:30:55 -0700

    Switch data structures, add error page
    Also a "now playing" page, though it's not fleshed out.

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 var Sonos = require('sonos'),
     Express = require('express'),
+    builder = require('xmlbuilder'),
+    SortedMap = require('collections/sorted-map'),
     app = Express();
 
+// Create the HTTP server
+var server = app.listen(3000, function() {
+   console.log('Listening on %d', server.address().port);
+});
+
+// Set the content type to text/xml
+app.use(function(req, res, next) {
+   res.contentType('text/xml');
+   next();
+});
+
 // our devices
-var devices = [];
+var devices = new SortedMap();
 
 // Serach for devices and push them into the pool.
 Sonos.search(function (device) {
@@ -11,17 +24,63 @@ Sonos.search(function (device) {
       // If it's not the bridge, push it into the pool.
       // We don't care about the bridge.
       if (data.CurrentZoneName != "BRIDGE") {
-         devices.push(device);
-         console.log("Pushed", device);
+         var player = { device : device, attributes : data };
+         devices.set(device.host.split('.').join(""), player);
+         console.log(JSON.stringify(player, null, 2));
       }
    });
 });
 
-app.get('/', function (req, res) {
-   var json = { players : devices };
-   res.json(json);
+var error = function (message) {
+   var root = builder.create('CiscoIPPhoneText');
+   root.ele('Title', 'Error!');
+   root.ele('Prompt', 'Something\'s gone wrong!');
+   root.ele('Text', message);
+   return root.end({ pretty : true });
+};
+
+app.get('/', function (req, res, next) {
+   var root = builder.create('CiscoIPPhoneMenu');
+
+   root.ele('Title', 'Cisnos Sonos controller');
+   root.ele('Prompt', 'Select a player:');
+
+   // Foreach device add a button.
+   devices.forEach(function (device, key) {
+      var item = root.ele('MenuItem');
+      item.ele('Name', device.attributes.CurrentZoneName)
+      item.ele('URL', "/" + key);
+   });
+
+   res.send(root.end({ pretty: true }));
 });
 
-var server = app.listen(3000, function() {
-   console.log('Listening on %d', server.address().port);
+app.get('/:id', function (req, res, next) {
+   var device = devices.get(req.params.id);
+   if (!device) {
+      res.status(404).send(error('No player found by that ID'));
+      return;
+   }
+
+   var root = builder.create('CiscoIPPhoneText');
+   root.ele('Title', device.attributes.CurrentZoneName);
+   root.ele('Prompt', 'Now Playing:');
+   root.ele('Text', 'Amy Winehouse');
+
+   var softkeys = [
+      { name : "play", action : "play" },
+      { name : "pause", action : "pause" },
+      { name : "v+", action : "volup" },
+      { name : "v-", action : "voldown"}
+   ];
+
+   for (var k in softkeys) {
+      var key = root.ele('SoftKeyItem');
+      key.ele('Name', softkeys[k].name);
+      key.ele('URL', "/" + req.params.id + "/" + softkeys[k].action);
+      key.ele('position', parseInt(k) + 1);
+   }
+
+   res.send(root.end({ pretty : true }));
 });
+
