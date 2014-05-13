0e36268e6a0d62cd30cacf10bd01774c7d4b854d
Scott Vanderlind <scottv@ifixit.com>
2014-05-13 10:42:52 -0700

    Create a pool of devices
    And keep them around

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var Sonos = require('sonos');
-var search = Sonos.search();
 
-search.on('DeviceAvailable', function (device, model) {
-   console.log(device, model);
+// our devices
+var devices = [];
+
+// Serach for devices and push them into the pool.
+Sonos.search(function (device) {
+   device.getZoneAttrs(function(err, data) {
+      // If it's not the bridge, push it into the pool.
+      // We don't care about the bridge.
+      if (data.CurrentZoneName != "BRIDGE") {
+         devices.push(device);
+         console.log("Pushed", device);
+      }
+   });
 });
+
