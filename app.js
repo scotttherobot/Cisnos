c22a62a0f76e98bcb6c60d3ad6b206629f343c1a
Scott Vanderlind <scottv@ifixit.com>
2014-05-16 13:28:19 -0700

    Finish playlist feature
    Now, you can select a playlist, it'll enqueue it, and then start playing it.
    Also, the status page now has the duration and current time and updates itself
    every 5 seconds.
    Also the playlist files have now moved.
    
    TODO: there's still an issue with some song titles having special characters?
    The song Que Veux Tu causes an XML parse error.

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ Sonos.search(function (device) {
    });
 });
 
-fs.exists("playlists.json", function (exists) {
+fs.exists("playlists/playlists.json", function (exists) {
    if (exists) {
-      fs.readFile("playlists.json", "utf-8", function (err, data) {
+      fs.readFile("playlists/playlists.json", "utf-8", function (err, data) {
          data = JSON.parse(data);
          for (var i = 0; i < data.length; i++ ) {
             playlists.push(data[i]);
@@ -100,31 +100,41 @@ app.get('/:id/:action?/:uri?', function (req, res, next) {
       res.status(404).send(error('No player found by that ID'));
       return;
    }
+   // Autorefresh the page, dropping the action, after 10 seconds.
+   res.setHeader('Refresh', "5; url=http://" + req.headers.host + "/" + req.params.id);
+
    // If there's an action to do, do it.
    if (req.params.action) {
       switch (req.params.action) {
+         // Plays a playlist
          case "pl":
             player.device.flush(function (err, flushed) {
                var playlist = playlists.get(parseInt(req.params.uri));
                var tracks = playlist.tracks;
+               var queued = 0;
                // queue every track, depending on the track type.
                for (var i = 0; i < tracks.length; i++) {
                   switch (tracks[i].type) {
                   case "spotify":
                      player.device.queueSpotify(tracks[i].uri, function (err, data) {
-                        console.log(err, data);
-                        // TOOD only do this when index = 0 
-                        // ALso investigate the 500 error that's getting returned
-                        player.device.play(function (err, playing) {
-                           console.log("play sent");
-                           console.log(err, playing);
-                        });
+                        queued = queued + 1;
+                        if (queued >= tracks.length) {
+                           console.log("Sending play... queued is", queued);
+                           player.device.play(function (err, playing) {
+                              console.log("play sent");
+                              console.log(err, playing);
+                           });
+                        }
                      });
                      break;
                   case "mp3":
                      player.device.queue(tracks[i].uri, function (err, queuedit) {
-                        if (i == 0) {
+                        queued = queued + 1;
+                        if (queued >= tracks.length) {
+                           console.log("Sending play... queued is", queued);
                            player.device.play(function (err, playing) {
+                              console.log("play sent");
+                              console.log(err, playing);
                            });
                         }
                      });
@@ -133,19 +143,30 @@ app.get('/:id/:action?/:uri?', function (req, res, next) {
                }
             });
             break;
-         case "spotify":
+         case "uri":
             if (req.params.uri) {
                console.log(req.params.uri);
-               player.device.flush(function (err, flushed) {
-                  player.device.queueSpotify(req.params.uri, function (err, data) {
-                     player.device.play(function (err, playing) {
-                     });
-                  });
+               player.device.play(req.params.uri, function (err, playing) {
+                  console.log(err, playing);
                });
             }
             break;
+         case "queuespot":
+            player.device.queueSpotify(req.params.uri, function (err, data) {
+               console.log(err, data);
+            });
+            break;
+         case "flush":
+            player.device.flush(function (err, flushed) {
+               console.log(err, flushed);
+            });
+            break;
+         case "stop":
+            player.device.stop(function (err, stopped) {
+               console.log(err, stopped);
+            });
+            break;
          case "play":
-            // If there's a URI, play it, if not, just resume playing.
             player.device.play(function (err, playing) {
             });
             break;
@@ -190,10 +211,20 @@ app.get('/:id/:action?/:uri?', function (req, res, next) {
          softkeys.push({ name : "pause", action : "pause" });
       }
       player.device.currentTrack(function (err, track) {
+         var curMin = Math.floor(parseInt(track.position) / 60);
+         var curSec = parseInt(track.position) - curMin * 60;
+         var totalMin = Math.floor(parseInt(track.duration) / 60);
+         var totalSec = parseInt(track.duration) - totalMin * 60;
+         //console.log(track);
          var root = builder.create('CiscoIPPhoneText');
          root.ele('Title', player.attributes.CurrentZoneName);
          root.ele('Prompt', 'Now Playing: ' + track.title);
-         root.ele('Text', "Track: " + track.title + "\nArtist: " + track.artist + "\nAlbum: " + track.album);
+         root.ele('Text', 
+            "Track: " + track.title + 
+            "\nArtist: " + track.artist + 
+            "\nAlbum: " + track.album +
+            "\nPosition: " + curMin + ":" + curSec + " / " + totalMin + ":" + totalSec  +
+            "\n");
 
          for (var k in softkeys) {
             var key = root.ele('SoftKeyItem');
